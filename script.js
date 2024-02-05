c72aa2037f2affc3a4c9824eda8aff667c6d8afd
davidson martins <davidsonxlife@hotmail.comgit config--global user.namedavidson>
2024-02-05 16:30:02 -0300

    star wars

---
+++ script.js
@@ -0,0 +1,173 @@
+let currentPageUrl='https://swapi.dev/api/people/'
+
+window.onload = async () => {
+    try {
+        await loadCharacters(currentPageUrl);
+    }
+    catch (error){
+        console.log(error);
+        alert ('erro ao carregar cards');
+    }
+
+        const nextButton = document.getElementById('next-button')
+        const backButton = document.getElementById('back-button')
+
+        nextButton.addEventListener('click',loadNextPage)
+        backButton.addEventListener('click',loadPreviousPage)
+        
+    
+};
+async function loadCharacters (url){
+    const mainContent=document.getElementById('main-content')
+    mainContent.innerHTML='';
+
+try {
+const response=  await fetch(url);
+const responseJson = await response.json();
+
+responseJson.results.forEach((character)=>{
+    const card = document.createElement("div")
+    card.style.backgroundImage = `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,"")}.jpg')`
+    card.className = "cards"
+
+    const characterNameBG = document.createElement("div")
+    characterNameBG.className = 'character-name-bg'
+
+    const characterName = document.createElement("span")
+    characterName.className = "character-name"
+    characterName.innerText =`${character.name}`
+
+    characterNameBG.appendChild(characterName)
+    card.appendChild(characterNameBG)
+
+    card.onclick = ()=>{
+        const modal = document.getElementById("modal")
+        modal.style.visibility ="visible"
+
+        const modalContent=document.getElementById("modal-content")
+        modalContent.innerHTML = ''
+
+        const characterImage = document.createElement("div")
+        characterImage.style.backgroundImage =
+        `url('https://starwars-visualguide.com/assets/img/characters/${character.url.replace(/\D/g,"")}.jpg')`
+        characterImage.className ="character-image"
+
+        const name = document.createElement("span")
+        name.className = "character-details"
+        name.innerText = `nome:${character.name}`
+
+        const characterHeight = document.createElement("span")
+        characterHeight.className = "character-details"
+        characterHeight.innerText = `altura:${convetHeight(character.height)}`
+
+        const mass= document.createElement("span")
+        mass.className = "character-details"
+        mass.innerText = `peso:${convertMass(character.mass)}`
+
+        const eyeColor= document.createElement("span")
+        eyeColor.className = "character-details"
+        eyeColor.innerText = `cor dos olhos:${ convertEyeColor(character.eye_color)}`
+
+        const birthyear= document.createElement("span")
+        birthyear.className = "character-details"
+        birthyear.innerText = `nascimento:${convetBirthYear(character.birth_year)}`
+
+        modalContent.appendChild(characterImage)
+        modalContent.appendChild(name)
+        modalContent.appendChild(characterHeight)
+        modalContent.appendChild(mass)
+        modalContent.appendChild(eyeColor)
+        modalContent.appendChild(birthyear)
+
+
+    }
+
+    mainContent.appendChild(card)
+});
+
+const nextButton = document.getElementById('next-button')
+const backButton = document.getElementById('back-button')
+
+nextButton.disabled = !responseJson.next
+backButton.disabled = !responseJson.previous
+
+backButton.style.visibility = responseJson.previous?"visible":"hidden"
+
+currentPageUrl = url
+
+}catch (error){
+    alert('erro ao carregar personagens')
+    console.log(error)
+}
+}
+async function loadNextPage() {
+    if (!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.next)
+
+    } catch (error){
+        console.log(error)
+        alert('erro ao carregar a proxima pagina')
+    }
+    
+}
+async function loadPreviousPage(){
+    if(!currentPageUrl) return;
+
+    try {
+        const response = await fetch(currentPageUrl)
+        const responseJson = await response.json()
+
+        await loadCharacters(responseJson.previous)
+    }catch (error){
+        console.log(error)
+        alert('erro ao carregar pagina anterior')
+    }
+    
+}
+function hidemodal(){
+    const modal = document.getElementById("modal")
+    modal.style.visibility = "hidden"
+}
+
+function convertEyeColor(eyeColor){
+    const cores = {
+        blue:"azul",
+        brown:"marron",
+        green:"verde",
+        yellow:"amarelo",
+        black:"preto",
+        pink:"rosa",
+        red:"vermelho",
+        orange:"laranja",
+        hazel:"avelã",
+        unknown:"desconheçido"
+    };
+    return cores[eyeColor.toLowerCase()]|| eyeColor;
+}
+function convetHeight(height){
+    if(height === "unknown"){
+        return "desconheçida"
+    }
+    return(height/100).toFixed(2);
+}
+function convertMass(mass){
+    if(mass ==="unknown"){
+        return"desconheçido"
+    }
+    return`${mass}kg`
+}
+function convetBirthYear(birthyear){
+    if(birthyear === "unknown"){
+        return "desconheçido"
+    }
+    return birthyear
+}
+
+
+
+
